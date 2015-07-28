26decac0427f55061e8b80314314334dc036ad34
Just me <>
2015-07-28 23:51:48 +0100

    Start adding tests. Also add NSubstitute dependency.

---
+++ Interval/Interval/ClosedIntervalOfLimitPointType.cs
@@ -0,0 +1,78 @@
+﻿namespace Interval
+{
+    using System.Collections.Generic;
+
+    public struct ClosedInterval<LimitPointType>
+        where LimitPointType : struct
+    {
+                public LimitPointType LowerLimitPoint
+        { get { return lowerLimitPoint; } }
+
+        public LimitPointType UpperLimitPoint
+        { get { return upperLimitPoint; } }
+        
+        public ClosedInterval(LimitPointType lowerLimitPoint, LimitPointType upperLimitPoint, IComparer<LimitPointType> comparesLimitPoints)
+
+        {
+            CheckComparerIsNotNull(comparesLimitPoints);
+
+            this.comparesLimitPoints = comparesLimitPoints;
+
+            if (comparesLimitPoints.Compare(x: lowerLimitPoint, y: upperLimitPoint) > 0)
+
+            {
+                this.lowerLimitPoint = upperLimitPoint;
+
+                this.upperLimitPoint = lowerLimitPoint;
+            }
+
+            else
+
+            {
+                this.lowerLimitPoint = lowerLimitPoint;
+
+                this.upperLimitPoint = upperLimitPoint;
+            }
+
+        }
+
+        public ClosedInterval(LimitPointType lowerLimitPoint, LimitPointType upperLimitPoint)
+
+            : this(lowerLimitPoint, upperLimitPoint, Comparer<LimitPointType>.Default)
+        { }
+
+        public static ClosedInterval<LimitPointType> From(LimitPointType lowerLimitPoint)
+        {
+
+            return new ClosedInterval<LimitPointType>(lowerLimitPoint: lowerLimitPoint, upperLimitPoint: lowerLimitPoint);
+        }
+
+        public ClosedInterval<LimitPointType> To(LimitPointType upperLimitPoint)
+        {
+            return new ClosedInterval<LimitPointType>(lowerLimitPoint: this.LowerLimitPoint, upperLimitPoint: upperLimitPoint);
+        }
+
+        public bool Contains(LimitPointType candidatePoint)
+        {
+            if (comparesLimitPoints.Compare(candidatePoint, lowerLimitPoint) < 0)
+                return false;
+
+            if (comparesLimitPoints.Compare(candidatePoint, upperLimitPoint) > 0)
+                return false;
+
+            return true;
+        }
+
+        private static void CheckComparerIsNotNull(IComparer<LimitPointType> comparesLimitPoints)
+        {
+            if (comparesLimitPoints == null)
+                throw new System.ArgumentNullException("We need something to compare limit points");
+        }
+
+        private IComparer<LimitPointType> comparesLimitPoints;
+
+        private readonly LimitPointType lowerLimitPoint;
+
+        private readonly LimitPointType upperLimitPoint;
+    }
+}
---
+++ Interval/IntervalTests/IntervalTests.cs
@@ -0,0 +1,37 @@
+﻿namespace IntervalTests
+{
+    using Interval;
+
+    using NUnit.Framework;
+    using NSubstitute;
+    using System.Collections.Generic;
+
+    [TestFixture]
+    public class ClosedIntervalTests
+    {
+        [Test]
+        public void WhenConstructorIsCalledWithLimitsWrongWayAround_ThenConstructedInstanceShouldHaveCorrectecLimits()
+        {
+            int wrongLowerLimit = 90;
+            int wrongUpperLimit = 10;
+
+            var classUnderTest = new ClosedInterval<int>(lowerLimitPoint: wrongLowerLimit, upperLimitPoint: wrongUpperLimit);
+
+            var limitComparisonResult = classUnderTest.LowerLimitPoint.CompareTo(classUnderTest.UpperLimitPoint);
+
+            Assert.That(limitComparisonResult, Is.LessThan(0), "Limit points should have been corrected.");
+        }
+
+        [Test]
+        public void WhenConstructorIsCalledWithComparer_ThenGivenComparerShouldBeCalled()
+        {
+            var mockComparer = Substitute.For<IComparer<int>>();
+
+            mockComparer.Compare(Arg.Any<int>(), Arg.Any<int>()).Returns(args => ((int)args[0]).CompareTo((int)args[1]));
+
+            var classUnderTest = new ClosedInterval<int>(lowerLimitPoint: 1, upperLimitPoint: 5, comparesLimitPoints: mockComparer);
+
+            mockComparer.ReceivedWithAnyArgs().Compare(1, 1);
+        }
+    }
+}
